6b09cb60d8f31d5b69f654f2a60df04f91bd1abb
Liam <xerbo@protonmail.com>
2021-04-07 12:15:51 +0100

    Improve code quality and telemetry parser
    Although for some reason the first letter of the callsign is stripped off

--- main.cpp
+++ main.cpp
@@ -1,24 +1,40 @@
+/*
+ * Lucky7-Decoder -  A small C++ program for decoding images from the Lucky-7 cubesat 
+ * Copyright (C) 2021 Xerbo (xerbo@protonmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <iostream>
 #include <fstream>
 #include <cstdint>
 #include <cstring>
 #include <vector>
-#include <bits/stdc++.h>
 
-#define FRAME_SIZE_BITS 280
-#define FRAME_SIZE_BYTES (FRAME_SIZE_BITS/8)
+#define FRAME_SIZE 35
 #define FRAME_DATA_SIZE 28
 
 struct Frame {
-	uint8_t Type;
+	uint8_t ID;
 	uint16_t Counter;
 	uint16_t Length;
 	uint8_t Data[FRAME_DATA_SIZE];
 };
 
 struct Telemetry {
-	char Callsign[7];
-	char SatelliteName[7];
+	std::string Callsign;
+	std::string SatelliteName;
 	uint16_t TotalResetCounter;
 	uint16_t SwapResetCounter;
 	uint8_t BatteryVoltage;
@@ -33,18 +49,21 @@ struct Telemetry {
 	bool AntennaBurnwire;
 };
 
-Frame ParseFrame(uint8_t *in){
+Frame parseFrame(uint8_t *in) {
 	Frame frame;
-	frame.Type     = in[0];
+	frame.ID       = in[0];
 	frame.Counter  = in[1] << 8 | in[2];
 	frame.Length   = in[5] << 8 | in[6];
 	std::memcpy(frame.Data, &in[7], FRAME_DATA_SIZE);
+
 	return frame;
 }
 
-void ParseTelemetry(Frame in){
+void parseTelemetry(Frame in) {
 	Telemetry telm;
 
+	telm.Callsign.append((char *)&in.Data[0], (char *)&in.Data[5]);
+	telm.SatelliteName.append((char *)&in.Data[5], (char *)&in.Data[11]);
 	telm.TotalResetCounter = in.Data[11] << 8 | in.Data[12];
 	telm.SwapResetCounter = in.Data[13] << 8 | in.Data[14];
 	telm.BatteryVoltage = in.Data[15];
@@ -58,26 +77,24 @@ void ParseTelemetry(Frame in){
 	telm.AngularRateZAxis = in.Data[25] << 8 | in.Data[26];
 	telm.AntennaBurnwire = in.Data[27] == 1;
 
-
 	std::cout << "Telemetry Packet:" << std::endl;
-	std::cout << "Callsign: " << telm.Callsign << std::endl;
-	std::cout << "Satellite Name: " << telm.SatelliteName << std::endl;
-	std::cout << "Total Reset Counter: " << (int)telm.TotalResetCounter << std::endl;
-	std::cout << "Swap Reset Counter: " << (int)telm.SwapResetCounter << std::endl;
-	std::cout << "Battery Voltage: " << telm.BatteryVoltage * 0.02f << "V" << std::endl;
-	std::cout << "MCU Temperature: " << (int)telm.MCUTemperature << "°C" << std::endl;
-	std::cout << "PA Temperature: " << (int)telm.PATemperature << "°C" << std::endl;
-	std::cout << "Processor Current: " << (int)telm.ProcessorCurrent << "mA" << std::endl;
-	std::cout << "MCU Voltage 3V3: " << telm.MCUVoltage3V3 * 0.02f << "V" << std::endl;
-	std::cout << "MCU Voltage 1V2: " << telm.MCUVoltage1V2 * 0.02f << "V" << std::endl;
-
-	std::cout << "Angular Rate X Axis: " << (telm.AngularRateXAxis == 2001 ? "Gyro Off" : std::to_string(telm.AngularRateXAxis)) << std::endl;
-	std::cout << "Angular Rate Y Axis: " << (telm.AngularRateYAxis == 2001 ? "Gyro Off" : std::to_string(telm.AngularRateYAxis)) << std::endl;
-	std::cout << "Angular Rate Z Axis: " << (telm.AngularRateZAxis == 2001 ? "Gyro Off" : std::to_string(telm.AngularRateZAxis)) << std::endl;
-	std::cout << "Antenna Burnwire: " << (telm.AntennaBurnwire ? "Enabled" : "Disabled") << std::endl << std::endl;
+	std::cout << "  Callsign: " << telm.Callsign << std::endl;
+	std::cout << "  Satellite Name: " << telm.SatelliteName << std::endl;
+	std::cout << "  Total Reset Counter: " << (int)telm.TotalResetCounter << std::endl;
+	std::cout << "  Swap Reset Counter: " << (int)telm.SwapResetCounter << std::endl;
+	std::cout << "  Battery Voltage: " << telm.BatteryVoltage * 0.02f << "V" << std::endl;
+	std::cout << "  MCU Temperature: " << (int)telm.MCUTemperature << "°C" << std::endl;
+	std::cout << "  PA Temperature: " << (int)telm.PATemperature << "°C" << std::endl;
+	std::cout << "  Processor Current: " << (int)telm.ProcessorCurrent << "mA" << std::endl;
+	std::cout << "  MCU Voltage 3V3: " << telm.MCUVoltage3V3 * 0.02f << "V" << std::endl;
+	std::cout << "  MCU Voltage 1V2: " << telm.MCUVoltage1V2 * 0.02f << "V" << std::endl;
+	std::cout << "  Angular Rate X Axis: " << (telm.AngularRateXAxis == 2001 ? "Gyro Off" : std::to_string(telm.AngularRateXAxis)) << std::endl;
+	std::cout << "  Angular Rate Y Axis: " << (telm.AngularRateYAxis == 2001 ? "Gyro Off" : std::to_string(telm.AngularRateYAxis)) << std::endl;
+	std::cout << "  Angular Rate Z Axis: " << (telm.AngularRateZAxis == 2001 ? "Gyro Off" : std::to_string(telm.AngularRateZAxis)) << std::endl;
+	std::cout << "  Antenna Burnwire: " << (telm.AntennaBurnwire ? "Enabled" : "Disabled") << std::endl << std::endl;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
 	if(argc != 3) {
 		std::cout << "Usage: " << argv[0] << " input.bin output.jpg" << std::endl;
 		return 1;
@@ -93,41 +110,31 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	uint8_t *Buffer = new uint8_t[FRAME_SIZE_BYTES];
-	std::vector<Frame> Frames;
+	// Up to 56kb of image data
+	Frame image_frames[2000];
+	size_t length = 0;
+
 	while (!data_in.eof()) {
-		data_in.read((char *)Buffer, FRAME_SIZE_BYTES);
-		Frames.push_back(ParseFrame(Buffer));
-		if(Frames[Frames.size()-1].Counter == 0x0000){
-			ParseTelemetry(Frames[Frames.size()-1]);
+		uint8_t buffer[FRAME_SIZE];
+		data_in.read((char *)buffer, FRAME_SIZE);
+
+		Frame frame = parseFrame(buffer);
+
+		if(frame.ID == 0x80 && frame.Counter == 0){
+			parseTelemetry(frame);
+		}
+		if (frame.ID == 0x80 && frame.Counter >= 0xC000) {
+			image_frames[frame.Counter - 0xC000] = frame;
+			length = frame.Length;	
 		}
 	}
 
-	// Behold, this ungodly mess of a sorting algorithm
-	std::vector<Frame> SortedFrames;
-	int FramesSize = Frames.size();
-	for(int j = 0; j < FramesSize; j++){
-		int Smallest = INT_MAX; int SmallestIndice = 0;
-		for(int i = 0; i < (int)Frames.size(); i++) {
-			if(Frames[i].Counter < Smallest){
-				Smallest = Frames[i].Counter;
-				SmallestIndice = i;
-			}
-		}
-
-		SortedFrames.push_back(Frames[SmallestIndice]);
-		Frames.erase(Frames.begin() + SmallestIndice);
+	for (size_t i = 0; i < length; i++) {
+		if (image_frames[i].ID != 0) data_out.write((char *)&image_frames[i].Data, FRAME_DATA_SIZE);
 	}
 
-	// Actually write the data, skipping duplicate frames
-	for(int i = 0; i < (int)SortedFrames.size(); i++) {
-		if(SortedFrames[i].Counter == SortedFrames[i+1].Counter) continue;
-
-		// We probably should check when the image transmission ends here, but unless you somehow get 2 images in a single pass that doesn't matter
-		if(SortedFrames[i].Type == 0x80 && SortedFrames[i].Counter >= 0xC000){
-			data_out.write((char *)SortedFrames[i].Data, FRAME_DATA_SIZE);
-		}
-	}
+	data_in.close();
+	data_out.close();
 
 	return 0;
 }
